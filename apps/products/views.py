84edb9430dddf1eb8ec0d5d4ecc4eef4c95cf235
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-02 23:23:16 +0600

    Add views(ProductModelViewSet, ProductsAPIView)

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from .serializers import *
+from services import get_all_products
 
-# Create your views here.
+
+class ProductModelViewSet(viewsets.ModelViewSet):
+    queryset = get_all_products()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+
+class ProductsAPIView(viewsets.ModelViewSet):
+    queryset = get_all_products()
+    serializer_class = ProductsSerializer
+    lookup_field = 'id'
