e07935f29d23983fbfc39f95f75e8dd576d5c5ad
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-05 20:13:28 +0600

    update(serializers, urls)

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,23 +1,29 @@
 from rest_framework import serializers
 from apps.products.models import *
 
-class ProductPhotoSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = ProductPhoto
-        fields = '__all__'
+# class ProductPhotoSerializer(serializers.ModelSerializer):
+#
+#     class Meta:
+#         model = ProductPhoto
+#         fields = ('photo')
 
 
 class ProductsSerializer(serializers.ModelSerializer):
-    product_photos = ProductPhotoSerializer(many=True, read_only=True, label='фото')
+    # product_photos = ProductPhotoSerializer(many=True, read_only=True, label='фото')
     class Meta:
         model = Product
-        fields = ('title', 'price', 'product_photos')
+        fields = ('title', 'price')
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    product_photos = ProductPhotoSerializer(many=True, read_only=True, label='фото')
+    # product_photos = ProductPhotoSerializer(many=True, read_only=True, label='фото')
 
     class Meta:
         model = Product
-        fields = ('title', 'description', 'price', 'available', 'category', 'product_photos')
+        fields = ('title', 'description', 'price', 'available', 'category')
+
+class ProductPhotoSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    class Meta:
+        model = ProductPhoto
+        fields = '__all__'
--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('products/', ProductsModelViewSet.as_view({'get': 'list'}), name="products"),
     path('product/<int:id>/', ProductModelViewSet.as_view({'get': 'retrieve'}), name="product"),
-    path('photo', PhotoModelViewSet.as_view({'get': 'list'}))
+    path('photo/', PhotoModelViewSet.as_view({'get': 'list'}))
 ]
