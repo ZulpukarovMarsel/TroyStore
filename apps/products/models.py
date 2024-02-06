386fb69c7ced590ad6cb84180f1834d0d7995825
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-06 23:34:09 +0600

    Update(models)

--- apps/products/models.py
+++ apps/products/models.py
@@ -4,6 +4,10 @@ from .constans import CM_DIRECTIONS, RUS_DIMENSION, EUR_DIMENSION, US_DIMENSION
 class Category(models.Model):
     title = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
     def __str__(self):
         return self.title
 
@@ -15,6 +19,11 @@ class Product(models.Model):
     category = models.ManyToManyField(Category, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
     def __str__(self):
         return f'Название:{self.title}, В налиичи{self.available}'
 
@@ -26,10 +35,18 @@ class ProductSize(models.Model):
     eur = models.IntegerField(choices=EUR_DIMENSION, blank=True, verbose_name="Размер ноги в eur")
     us = models.IntegerField(choices=US_DIMENSION,blank=True, verbose_name="Размер ноги в us")
 
+    class Meta:
+        verbose_name = "Размер"
+        verbose_name_plural = "Размеры"
+
 
 class ProductPhoto(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='photos')
     photo = models.ImageField(upload_to='photo_product')
+
+    class Meta:
+        verbose_name = "Фото продукты"
+        verbose_name_plural = "Фото продуктов"
 
     def __str__(self):
         return f'Фото продукта {self.product.title}'
@@ -38,6 +55,9 @@ class Delivery(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='delivery')
     description = models.TextField()
 
+    class Meta:
+        verbose_name = "Доставка"
+
     def __str__(self):
         return f'Доставка продукта {self.product.title}'
 
@@ -45,6 +65,10 @@ class ProductCharacteristic(models.Model):
     product = models.ForeignKey(Product, related_name='characteristics', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     value = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "характеристика продукта"
+        verbose_name_plural = "характеристика продуктов"
 
     def __str__(self):
         return f'{self.name}: {self.value} for {self.product.title}'
