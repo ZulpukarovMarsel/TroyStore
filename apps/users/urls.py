6c3aaf6bd7b69dfaaaa7aa21cddfe0b2f396e6c8
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-09 23:06:58 +0600

    update settings, urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import UserProfileView, UserRegistrationAPIView
+from .views import UserProfileView, UserRegistrationAPIView, UserLoginAPIView, LogoutAPIView
 
 urlpatterns = [
     # path('accounts/', include('allauth.urls')),
     path('profile/', UserProfileView.as_view()),
     path('sign_up/', UserRegistrationAPIView.as_view()),
-
+    path('sign_in/', UserLoginAPIView.as_view()),
+    path('sign_out/', LogoutAPIView.as_view()),
 ]
--- config/settings/base.py
+++ config/settings/base.py
@@ -23,6 +23,7 @@ ROOT_URLCONF = 'config.urls'
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    'rest_framework_simplejwt.token_blacklist',
     "drf_yasg",
     "django_filters",
     'allauth',
@@ -58,9 +59,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    # "DEFAULT_AUTHENTICATION_CLASSES": (
-    #     "rest_framework_simplejwt.authentication.JWTAuthentication",
-    # ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
