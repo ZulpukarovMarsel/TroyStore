5ebc06686b94b5fd7c8ee9bf7768b2e68f31464d
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-13 23:01:36 +0600

    testing favorite

--- apps/products/tests.py
+++ apps/products/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from .models import Product, UserFavoriteProduct
 
-# Create your tests here.
+class FavoritesTestCase(TestCase):
+    def setUp(self):
+        # Создаем пользователя
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+
+        # Создаем продукт
+        self.product = Product.objects.create(title='Test Product')
+
+        # Создаем API-клиент
+        self.client = APIClient()
+
+        # Логинимся под созданным пользователем
+        self.client.login(username='testuser', password='testpass')
+
